265686d3eb15f1a32a211296019016958956f47c
Petar Gregurek <pgregurek@gmail.com>
2026-04-01 16:58:15 +0200

    setup

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = 10;
+        int number2 = 20;
+        int sum = number1 + number2;
+
+        Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+    }
+}
